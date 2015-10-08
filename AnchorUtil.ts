5a8d02a582dd9d0735bbd9f47547d8f786a873cc
Saco <286844968@qq.com>
2015-10-08 19:33:53 +0800

    update

--- AnchorUtil.ts
+++ AnchorUtil.ts
@@ -1,9 +1,19 @@
 /**
  * 该工具类用于解决EgretEngine2.5版本没有anchorX/anchorY属性值的问题
- * 在创建游戏场景前需要执行AnchorUtil.init()初始化工具并完成属性的注入
- * AnchorUtil.setAnchorX(target, anchorX) 设置对象的anchorX值
- * AnchorUtil.setAnchorY(target, anchorY) 设置对象的anchorY值
- * AnchorUtil.setAnchor(target, anchor) 同时设置对象的anchorX和anchorY值
+ * 在创建游戏场景前需要执行AnchorUtil.init();初始化工具并完成属性的注入
+ * 方式一（推荐）：
+ * AnchorUtil.setAnchorX(target, anchorX); //设置对象的anchorX值
+ * AnchorUtil.setAnchorY(target, anchorY); //设置对象的anchorY值
+ * AnchorUtil.setAnchor(target, anchor); //同时设置对象的anchorX和anchorY值
+ * 方式二：
+ * target["anchorX"] = value; //设置对象的anchorX值
+ * target["anchorY"] = value; //设置对象的anchorY值
+ * target["anchor"] = value; //同时设置对象的anchorX和anchorY值
+ * 方式三：
+ * 修改egret.d.ts，在DisplayObject声明中添加anchorX、anchorY和anchor属性，代码的写法和引擎之前版本相同：
+ * target.anchorX = value; //设置对象的anchorX值
+ * target.anchorY = value; //设置对象的anchorY值
+ * target.anchor = value; //同时设置对象的anchorX和anchorY值
  *
  * Created by Saco on 2015/9/16.
  */
@@ -123,6 +133,23 @@ class AnchorUtil{
                 return this["_anchorY"];
             },
             set: function (value) {
+                this._anchorY = value;
+                AnchorUtil._propertyChange[this.hashCode] = true;
+                AnchorUtil._anchorChange[this.hashCode] = true;
+                egret.callLater(()=>{
+                    AnchorUtil.changeAnchor(this);
+                }, this);
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        Object.defineProperty(egret.DisplayObject.prototype, "anchor", {
+            get: function () {
+                return this["_anchorX"];
+            },
+            set: function (value) {
+                this._anchorX = value;
                 this._anchorY = value;
                 AnchorUtil._propertyChange[this.hashCode] = true;
                 AnchorUtil._anchorChange[this.hashCode] = true;
